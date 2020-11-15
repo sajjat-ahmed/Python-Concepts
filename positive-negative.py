16264529cbe05a63cbbff302f593843378e919e1
sajjat-ahmed <ahm.sajjat@gmail.com>
2020-11-15 20:39:53 +0600

    find positive negative number programe

---
+++ positive-negative.py
@@ -0,0 +1,8 @@
+number = int(input('Type number: '))
+
+if number < 0:
+    print("This number is Negative")
+elif number > 0:
+    print("This number is Posttive")
+else:
+    print("This number is Zeor!")
