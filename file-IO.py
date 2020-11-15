88101b49399a4249afbd65e205a41f14e6480d7f
sajjat-ahmed <ahm.sajjat@gmail.com>
2020-11-15 21:11:44 +0600

    python file read wright programe

--- file-IO.py
+++ file-IO.py
@@ -3,6 +3,14 @@ with open("text.txt", mode="r") as s_file:
     for line in s_file.readlines():
         word = line.strip().split(" ")
         words_all += word
-    print(words_all)
+    
+    unique_word = set(words_all)
+    print(len(unique_word))
+    print(len(words_all))
+    print(unique_word)
+
+    with open("unique_words.txt", mode="w") as write_file:
+        for item in unique_word:
+            write_file.write(item)
 
 print("Finished")
