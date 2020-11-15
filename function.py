62e7423a9cffee6ff4ae95e837a0beaf923f955d
sajjat-ahmed <ahm.sajjat@gmail.com>
2020-11-15 20:47:57 +0600

    learn python function

---
+++ function.py
@@ -0,0 +1,8 @@
+def add_two_values(first, second):
+    return first + second
+
+number_1 = input("Type number: ")
+number_2 = input("Type Number: ")
+sum = add_two_values(number_1, number_2)
+
+print(sum)
