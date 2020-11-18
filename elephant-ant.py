69efd17c4f7081db4cc26f7ad8f5732e215c2449
Sajjat Ahmed <67552370+sajjat-ahmed@users.noreply.github.com>
2020-11-18 20:56:22 +0600

    Create elephant-ant.py

---
+++ elephant-ant.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.bgcolor("white")
+turtle.pensize(10)
+turtle.speed(1)
+
+def circle():
+    turtle.circle(100)
+
+elephant_weight = 3000
+ant_weight = 0.1
+
+if elephant_weight > ant_weight:
+    circle()
+
+
+turtle.hideturtle()
